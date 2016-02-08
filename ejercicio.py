52ca5482a6ce8a94ac80d0f68e04b9e615862348
Laura Trabas Clavero <ltrabas@zeta40.aulas.gsyc.es>
2016-02-08 10:53:04 +0100

    ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
+fd = open("/etc/passwd","r")
+
+lineas = fd.readlines()
+
+diccionario = {}
+
+for linea in lineas:
+	caracteres = linea.split(":")
+	diccionario[caracteres[0]] = caracteres[-1][:-1]
+
+try:
+
+	print diccionario["root"]
+	print diccionario["imaginario"]
+	
+except KeyError:
+	print("El nombre no existe")
+
+fd.close()
 
